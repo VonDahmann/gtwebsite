9b8114555a42413240e9bff6432c36d99aba438a
VonDahmann <179260444+VonDahmann@users.noreply.github.com>
2026-04-16 00:26:35 +0100

    fix: switch to docs folder for Github Pages

--- app/vite.config.js
+++ app/vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/gtwebsite/', // Ensure this matches your repository name
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 })
